eb004629f8987e8be1cea7552e9018a3bcbcd50e
JonathamCarvalho <jonatham.carvalho@gmail.com>
2024-12-24 18:57:09 -0300

    Sorteio

---
+++ script.js
@@ -0,0 +1,13 @@
+const sectionButton = document.querySelector(".section-button")
+
+function generateNumber(){
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+}
+
+
+sectionButton.addEventListener("click", generateNumber)
